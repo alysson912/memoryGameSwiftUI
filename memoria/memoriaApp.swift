8b25fa3d6dcfbe74eb517971e469d13647588670
Alysson Menezes <almenezes912@gmail.com>
2022-01-10 15:21:21 -0300

    Initial Commit

---
+++ memoria/memoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoriaApp.swift
+//  memoria
+//
+//  Created by ALYSSON MENEZES  on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct memoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
